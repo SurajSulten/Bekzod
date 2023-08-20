7002cf6894444bfe35b0e3689ba36fe9840fb14d
Suroj <suraj.sulten@gmail.com>
2023-08-21 00:15:05 +0500

    fix a href

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -31,7 +31,7 @@ const Experience = () => {
               <BsPatchCheckFill className='experience__details-icon' />
               <div>
                 <h4>Ziyookline</h4>
-                <a className='text-light' href="https://ziyookline.uz/ru/" target='_blank'>https://ziyookline.uz/ru/</a>
+                <a className='text-light' href="https://ziyookline.uz/ru/" target='_blank' rel="noreferrer">https://ziyookline.uz/ru/</a>
               </div>
             </article>
           </div>
@@ -44,7 +44,7 @@ const Experience = () => {
               <BsPatchCheckFill className='experience__details-icon' />
               <div>
                 <h4>Eurosoft</h4>
-                <a className='text-light' href="https://eurosoft.uz/#/" target='_blank'>https://eurosoft.uz/#/</a>
+                <a className='text-light' href="https://eurosoft.uz/#/" target='_blank' rel="noreferrer">https://eurosoft.uz/#/</a>
               </div>
             </article>
           </div>
@@ -57,7 +57,7 @@ const Experience = () => {
               <BsPatchCheckFill className='experience__details-icon' />
               <div>
                 <h4>Adliya Vazirligi</h4>
-                <a className='text-light' href="https://adliya.uz/" target='_blank'>https://adliya.uz/</a>
+                <a className='text-light' href="https://adliya.uz/" target='_blank' rel="noreferrer">https://adliya.uz/</a>
               </div>
             </article>
           </div>
